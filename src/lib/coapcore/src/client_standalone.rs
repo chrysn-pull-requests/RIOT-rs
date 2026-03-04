4a5674dfc0d4cef5fda65bac6e95ba71fe7b06c4
chrysn <chrysn@fsfe.org>
2026-03-04 08:29:48 +0100

    Simplify ICE example

--- src/lib/coapcore/src/client_standalone.rs
+++ src/lib/coapcore/src/client_standalone.rs
@@ -20,57 +20,22 @@ where
     WS: Stack + 'ws,
 {
     wire: WS,
-    security_config: SecurityConfig,
-    state: (),
     _phantom: PhantomData<&'ws ()>, // FIXME or other?
-}
-
-struct SecurityConfig {
-    own_credential: lakers::Credential,
-    own_key: lakers::BytesP256ElemLen,
-    own_credential_transfer: lakers::CredentialTransfer,
-    peer_credential: lakers::Credential,
-}
-
-impl<'ws, WS> ClientSecurityWrapper<'ws, WS>
-where
-    WS: Stack + 'ws,
-{
-    /// Creates all the client state needed to send requests, and stores the credentials.
-    pub fn new(
-        wire: WS,
-        own_credential: lakers::Credential,
-        own_key: lakers::BytesP256ElemLen,
-        own_credential_transfer: lakers::CredentialTransfer,
-        peer_credential: lakers::Credential,
-    ) -> Self {
-        Self {
-            wire,
-            security_config: SecurityConfig {
-                own_credential,
-                own_key,
-                own_credential_transfer,
-                peer_credential,
-            },
-            state: (),
-            _phantom: PhantomData,
-        }
-    }
 }
 
 impl<'ws, WS> Stack for ClientSecurityWrapper<'ws, WS>
 where
     WS: Stack + 'ws,
 {
-    type RequestUnionError = <liboscore::ProtectedMessage as MinimalWritableMessage>::UnionError; // WS::RequestUnionError; // FIXME: probably more …
+    type RequestUnionError = core::convert::Infallible; // WS::RequestUnionError; // FIXME: probably more …
 
     type RequestMessage<'a>
-        = liboscore::ProtectedMessage
+        = core::convert::Infallible
     where
         Self: 'a;
 
     type ResponseMessage<'a>
-        = liboscore::ProtectedMessage
+        = core::convert::Infallible
     where
         Self: 'a;
 
@@ -80,13 +45,7 @@ where
         &mut self,
         request: Req,
     ) -> Result<Req::Output, Self::TransportError> {
-        self.wire
-            .request(RequestWrapper {
-                security_config: self.security_config,
-                request,
-                _phantom: PhantomData,
-            })
-            .await
+        todo!()
     }
 }
 
@@ -95,7 +54,6 @@ where
     R: Request<WS>,
     WS: Stack + 'ws,
 {
-    security_config: SecurityConfig,
     request: R,
     // FIXME can we do w/o?
     _phantom: PhantomData<&'ws WS>,
