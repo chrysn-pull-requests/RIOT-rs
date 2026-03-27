d4d622356b9faf4c206dde53cd96940bf5659811
chrysn <chrysn@fsfe.org>
2026-03-27 16:12:02 +0100

    drop async version of smart pixel driving

--- examples/pixelgraphics/src/graphicsdriver.rs
+++ examples/pixelgraphics/src/graphicsdriver.rs
@@ -24,36 +24,6 @@ pub(crate) static SIGNAL: Signal<CriticalSectionRawMutex, ()> = Signal::new();
 pub(crate) static PIXELS: BlockingMutex<CriticalSectionRawMutex, Cell<[RGB8; super::N_LEDS]>> =
     BlockingMutex::new(Cell::new([RGB8::new(0, 0, 0); super::N_LEDS]));
 
-#[cfg(false)]
-#[ariel_os::task(autostart, peripherals)]
-async fn matrix_refresh_async(peripherals: PixelPeripherals) {
-    info!("matrix refresh started");
-
-    use esp_hal::rmt::Rmt;
-    use esp_hal_smartled::{SmartLedsAdapterAsync, smart_led_buffer};
-    use smart_leds_trait::SmartLedsWriteAsync;
-
-    cfg_if::cfg_if! {
-        if #[cfg(feature = "esp32h2")] {
-            let freq = Rate::from_mhz(32);
-        } else {
-            let freq = Rate::from_mhz(80);
-        }
-    };
-
-    let rmt = Rmt::new(peripherals.rmt, freq).unwrap().into_async();
-    let mut rmt_buffer = smart_led_buffer!(super::N_LEDS + 20);
-    let mut led = SmartLedsAdapterAsync::new(rmt.channel0, peripherals.matrix, &mut rmt_buffer);
-
-    loop {
-        SIGNAL.wait().await;
-        let pixels = PIXELS.lock(|pixels| pixels.get());
-        if let Err(e) = led.write(pixels).await {
-            error!("Driving LED: {:?}", e);
-        }
-    }
-}
-
 #[ariel_os::task(autostart, peripherals)]
 async fn matrix_refresh_blocking_the_executor(peripherals: PixelPeripherals) {
     info!("matrix refresh started");
@@ -77,7 +47,20 @@ async fn matrix_refresh_blocking_the_executor(peripherals: PixelPeripherals) {
     loop {
         SIGNAL.wait().await;
         let pixels = PIXELS.lock(|pixels| pixels.get());
-        // Delibertely not awaiting: We *need* to do this continuously
+        // There is an implementation of SmartLedsAdapter in esp_hal_smartled that works async'ly.
+        // However, that only works when the async task gets the chance to feed the RMT peripheral
+        // fast enough -- and especially when WiFi is active, that is *not* the case, and the
+        // display glitches more the more traffic there is.
+        //
+        // The display even glitches on traffic in the blocking version, unless it is run in a
+        // critical section.
+        //
+        // The need for the update to run in a critical section justifies a simplification on the
+        // rest of the code (no need to wait for ariel_os::thread(peripherals), and just using
+        // `SIGNAL.wait().await`): Running in a critical section, we're blocking the main executor,
+        // but we'd just as well do that if we ran in a thread. And as there are no downsides,
+        // let's pick this simpler version, and on top of it we also don't need stack space for
+        // another task.
         critical_section::with(|_| {
             if let Err(e) = led.write(pixels) {
                 error!("Driving LED: {:?}", e);
